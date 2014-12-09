9976d9218a4294cdbbf3513fa8769c23253591d7
Adam J Nowak <adam@hyperspatial.com>
2014-12-09 12:15:04 -0700

    Remove dash widget

--- includes/functions-custom.php
+++ includes/functions-custom.php
@@ -33,32 +33,4 @@ $ter_child_shortcodes = array('[gray-box]' => 'A gray box with rounded corners')
 //Main section
 $ter_child_help = array('Help Section Title' => '<p>This is a paragraph of help text</p>','Help Section Title 2' => '<p>This is a paragraph of help text 2</p>');
 */
-
-/* <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~< A-Train Admin Branding >~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~> */
-
-/* Dashboard Feed ~~~~~~~~~~> */
-function ter_add_dashboard_meta(){ add_meta_box('atrain_feed','A-Train Marketing News','ter_add_dashboard_feed_html','dashboard','side','high'); }
-add_action('wp_dashboard_setup','ter_add_dashboard_meta');
-
-function ter_add_dashboard_feed_html($post,$callback_args){
-	$rss = fetch_feed('http://atrainmarketing.com/category/marketing-2/feed/');
-	if(!is_wp_error($rss)){
-   		$maxitems = $rss->get_item_quantity(5);
-		$rss_items = $rss->get_items(0,$maxitems);
-	}
-	?>
-	<ul>
-    <?php if($maxitems == 0): ?><li><?php _e('No Feed Items','terra') ?></li>
-	<?php else: ?>
-	<?php foreach($rss_items as $item): ?><li><a href="<?php echo esc_url($item->get_permalink()) ?>" title="<?php printf(__('Posted %s','terra'),$item->get_date('j F Y')) ?>" target="_blank"><?php echo esc_html($item->get_title()) ?></a></li><?php endforeach; ?>
-    <?php endif ?>
-	</ul>
-	<hr style="margin-bottom:10px">
-	<div style="text-align:center"><a href="http://atrainmarketing.com/category/marketing-2/" target="_blank">More Marketing News</a> | <a href="http://atrainmarketing.com/category/marketing-2/feed/" target="_blank">Subscribe to Feed</a></div>
-	<?php
-}
-/* <~~~~~~~~~~< END Dashboard Feed */
-
-/* Admin Footer ~~> */
-function ter_admin_footer(){ echo '<strong>ExpressLine Theme by <a href="http://atrainmarketing.com" target="_blank"><img src="' . TER_CHILD_GRAPHICS . 'icon-atrain.gif" alt="A-Train" style="vertical-align:text-bottom; margin:0 6px">A-Train Marketing</a></strong>'; }
 ?>
