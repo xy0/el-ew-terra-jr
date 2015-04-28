74ef8699f5e707a4d29cac57b6d01bb4173e0bca
Adam J Nowak <adam@hyperspatial.com>
2015-04-28 11:30:55 -0600

    text change cpt

--- includes/custom-post-type-skeleton.php
+++ includes/custom-post-type-skeleton.php
@@ -1,13 +1,13 @@
 <?php  /* <~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~< Skeleton >~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~> */
 
 //Create your Skeleton CPT - Use custom-post-type.php for reference
-$skeleton = new Skeleton('skeleton',array('post_type' => 'skeleton','name_singular' => 'Skeleton','name_plural' => 'Skeletons','icon' => TER_CHILD_GRAPHICS . 'favicon-16x16.png'));
+$skeleton = new Skeletons('skeletons',array('post_type' => 'skeletons','name_singular' => 'Skeleton','name_plural' => 'Skeletons','icon' => TER_CHILD_GRAPHICS . 'favicon-16x16.png'));
 
-class Skeleton extends TerCustomPostType{
+class Skeletons extends TerCustomPostType{
 	public function __construct($namespace,$config){
-		 parent::__construct($namespace,$config);
-		 $this->register_taxonomy(); //Optional, creates namespaced taxonomy. Pass argument true to make hierarchal
-		 $this->setup_meta_boxes(); //Optional, creates meta boxes, overwrite methods in your subclass
+		parent::__construct($namespace,$config);
+		$this->register_taxonomy(); //Optional, creates namespaced taxonomy. Pass argument true to make hierarchal
+		$this->setup_meta_boxes(); //Optional, creates meta boxes, overwrite methods in your subclass
 	}
 	//Subclass Methods and Overwrites
 	//Overwriting the init function is helpful if you need to change setup options.
